9e167e60537bc26b588d21a27a14ec269f05822c
jm2dev <jomarcar@gmail.com>
2013-12-17 01:35:17 +0000

    Testing a filter.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,20 +17,25 @@ angular.module('yoAngularjsTutorialApp')
       $scope.notes = notesFactory.get();
     };
   })
- .factory('notesFactory', function() {
-  return {
-    put: function(note) {
-      localStorage.setItem('todo' + (Object.keys(localStorage).length + 1), note);
-    },
-    get: function() {
-      var notes = [];
-      var keys = Object.keys(localStorage);
+  .factory('notesFactory', function() {
+    return {
+      put: function(note) {
+        localStorage.setItem('todo' + (Object.keys(localStorage).length + 1), note);
+      },
+      get: function() {
+        var notes = [];
+        var keys = Object.keys(localStorage);
 
-      for(var i = 0; i < keys.length; i++) {
-        notes.push(localStorage.getItem(keys[i]));
+        for(var i = 0; i < keys.length; i++) {
+          notes.push(localStorage.getItem(keys[i]));
+        }
+
+        return notes;
       }
-
-      return notes;
-    }
-  };
-});
+    };
+  })
+  .filter('truncate', function() {
+    return function(input,length) {
+      return (input.length > length ? input.substring(0, length) : input );
+    };
+  });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -123,3 +123,13 @@ describe('notesFactory tests', function() {
     expect(result.length).toBe(4);
   });
 });
+
+describe('filter tests', function() {
+  beforeEach(module('yoAngularjsTutorialApp'));
+  it('should truncate the input to 10 characters',
+    //this is how we inject a filter by appending Filter to the end of the filter name
+    inject(function(truncateFilter) {
+      expect(truncateFilter('abcdefghijkl', 10).length).toBe(10);
+    })
+  );
+});
